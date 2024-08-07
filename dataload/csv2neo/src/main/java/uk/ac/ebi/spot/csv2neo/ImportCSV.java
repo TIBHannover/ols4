653162badb1963866b7bfa1ea8ad2ee0b1f9439a
Erhun Giray TUNCAY <48091473+giraygi@users.noreply.github.com>
2024-08-07 22:30:07 +0200

    batch based solution is implemented for query based data creation in #25

--- dataload/csv2neo/src/main/java/uk/ac/ebi/spot/csv2neo/ImportCSV.java
+++ dataload/csv2neo/src/main/java/uk/ac/ebi/spot/csv2neo/ImportCSV.java
@@ -6,11 +6,7 @@ import org.neo4j.driver.*;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-
+import java.util.*;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
@@ -41,64 +37,63 @@ public class ImportCSV {
         return fileList;
     }
 
-    public static void generateCreationQueries(List<File> files, Session session, boolean safe) throws IOException {
-
+    public static void executeBatchedNodeQueries(List<File> files, Session session, int batchSize) throws IOException {
         for (File file : files) {
             if (!(file.getName().contains("_ontologies") || file.getName().contains("_properties")
                     || file.getName().contains("_individuals") || file.getName().contains("_classes")) || !file.getName().endsWith(".csv"))
                 continue;
-
             Reader reader = Files.newBufferedReader(Paths.get(file.getAbsolutePath()));
             org.apache.commons.csv.CSVParser csvParser = new org.apache.commons.csv.CSVParser(reader, CSVFormat.POSTGRESQL_CSV.withFirstRecordAsHeader().withTrim());
             String[] headers = csvParser.getHeaderNames().toArray(String[]::new);
-            for (CSVRecord csvRecord : csvParser) {
-                String[] row = csvRecord.toList().toArray(String[]::new);
-                String query = generateBlankNodeCreationQuery(headers,row);
-                Map<String,Object> params = generateProps(headers,row);
-                if(query.isEmpty())
-                    System.out.println("empty query for appended line: "+Arrays.toString(row)+" in file: "+file);
-                executeQuery(session, params, safe, query);
+            List<List<CSVRecord>> splitRecords = splitList(csvParser.getRecords(),batchSize);
+            for (List<CSVRecord> records : splitRecords){
+                try (Transaction tx = session.beginTransaction()){
+                    for (CSVRecord csvRecord : records) {
+                        String[] row = csvRecord.toList().toArray(String[]::new);
+                        String query = generateBlankNodeCreationQuery(headers,row);
+                        Map<String,Object> params = generateProps(headers,row);
+                        if(query.isEmpty())
+                            System.out.println("empty query for appended line: "+Arrays.toString(row)+" in file: "+file);
+                        else
+                            tx.run(query,params);
+                    }
+                    tx.commit();
+                }
             }
         }
+    }
 
-        for (File file : files){
-            if((!file.getName().contains("_edges")) || !file.getName().endsWith(".csv"))
+    public static void executeBatchedRelationshipQueries(List<File> files, Session session, int batchSize) throws IOException {
+        for (File file : files) {
+            if ((!file.getName().contains("_edges")) || !file.getName().endsWith(".csv"))
                 continue;
 
             Reader reader = Files.newBufferedReader(Paths.get(file.getAbsolutePath()));
             org.apache.commons.csv.CSVParser csvParser = new org.apache.commons.csv.CSVParser(reader, CSVFormat.POSTGRESQL_CSV.withFirstRecordAsHeader().withTrim());
             String[] headers = csvParser.getHeaderNames().toArray(String[]::new);
-
-            for (CSVRecord csvRecord : csvParser) {
-                String[] row = csvRecord.toList().toArray(String[]::new);
-                String query = generateRelationCreationQuery(headers,row);
-                if(query.isEmpty())
-                    System.out.println("empty query for appended line: "+Arrays.toString(row)+" in file: "+file);
-                executeQuery(session, null, safe, query);
+            List<List<CSVRecord>> splitRecords = splitList(csvParser.getRecords(), batchSize);
+            for (List<CSVRecord> records : splitRecords){
+                try (Transaction tx = session.beginTransaction()){
+                    for (CSVRecord csvRecord : records) {
+                        String[] row = csvRecord.toList().toArray(String[]::new);
+                        String query = generateRelationCreationQuery(headers,row);
+                        if(query.isEmpty())
+                            System.out.println("empty query for appended line: "+Arrays.toString(row)+" in file: "+file);
+                        else
+                            tx.run(query);
+                    }
+                    tx.commit();
+                }
             }
         }
     }
 
-    private static void executeQuery(Session session, Map<String,Object> params, boolean safe, String query){
-        if(safe){
-            try (Transaction tx = session.beginTransaction()) {
-                if (params != null)
-                    tx.run(query, params);
-                else
-                    tx.run(query);
-                tx.commit();
-            } catch(Exception e){
-                e.printStackTrace();
-            }
-        } else
-            try{
-                if (params != null)
-                    session.run(query, params);
-                else
-                    session.run(query);
-            } catch (Exception e){
-                e.printStackTrace();
-            }
+    public static <T> List<List<T>> splitList(List<T> list, int batchSize) {
+        List<List<T>> subLists = new ArrayList<>();
+        for (int i = 0; i < list.size(); i += batchSize) {
+            subLists.add(new ArrayList<>(list.subList(i, Math.min(i + batchSize, list.size()))));
+        }
+        return subLists;
     }
 
     private static Options getOptions() {
@@ -111,7 +106,7 @@ public class ImportCSV {
         options.addOption("uri", "databaseuri",true, "neo4j database uri");
         options.addOption("db", "database",true, "neo4j database name");
         options.addOption("d", "directory",true, "neo4j csv import directory");
-        options.addOption("s", "safe",false, "execute each neo4j query in transactions or the session");
+        options.addOption("bs", "batchsize",true, "batch size for splitting queries into multiple transactions.");
         return options;
     }
 
@@ -125,25 +120,40 @@ public class ImportCSV {
         final String dbPassword = cmd.hasOption("pw") ? cmd.getOptionValue("pw") : "testtest";
         final String directory = cmd.hasOption("d") ? cmd.getOptionValue("d") : "/tmp/out";
         final String ontologiesToBeRemoved = cmd.hasOption("rm") ? cmd.getOptionValue("rm") : "";
+        final int batchSize = cmd.hasOption("bs") && Integer.parseInt(cmd.getOptionValue("bs"))>0 ? Integer.parseInt(cmd.getOptionValue("bs")) : 1000;
 
         try (var driver = cmd.hasOption("a") ? GraphDatabase.driver(dbUri, AuthTokens.basic(dbUser, dbPassword)) : GraphDatabase.driver(dbUri)) {
             driver.verifyConnectivity();
             try (var session = driver.session(SessionConfig.builder().withDatabase(db).build())) {
-                try{
-                    session.run("CREATE CONSTRAINT IF NOT EXISTS FOR (n:Ontology) REQUIRE n.id IS UNIQUE");
-                    session.run("CREATE CONSTRAINT IF NOT EXISTS FOR (n:OntologyEntity) REQUIRE n.id IS UNIQUE");
-                    session.run("CREATE CONSTRAINT IF NOT EXISTS FOR (n:OntologyClass) REQUIRE n.id IS UNIQUE");
-                } catch(Exception e){
-                    e.printStackTrace();
-                }
+                List<String> indexCommands = new ArrayList<>();
+                indexCommands.add("CREATE CONSTRAINT IF NOT EXISTS FOR (n:Ontology) REQUIRE n.id IS UNIQUE");
+                indexCommands.add("CREATE CONSTRAINT IF NOT EXISTS FOR (n:OntologyEntity) REQUIRE n.id IS UNIQUE");
+                indexCommands.add("CREATE CONSTRAINT IF NOT EXISTS FOR (n:OntologyClass) REQUIRE n.id IS UNIQUE");
+                indexCommands.add("CREATE CONSTRAINT IF NOT EXISTS FOR (n:OntologyProperty) REQUIRE n.id IS UNIQUE");
+                indexCommands.add("CREATE CONSTRAINT IF NOT EXISTS FOR (n:OntologyIndividual) REQUIRE n.id IS UNIQUE");
+                indexCommands.add("CREATE TEXT INDEX ontology_id IF NOT EXISTS FOR (n:Ontology) ON (n.id)");
+                indexCommands.add("CREATE TEXT INDEX entity_id IF NOT EXISTS FOR (n:OntologyEntity) ON (n.id)");
+                indexCommands.add("CREATE TEXT INDEX class_id IF NOT EXISTS FOR (n:OntologyClass) ON (n.id)");
+                indexCommands.add("CREATE TEXT INDEX property_id IF NOT EXISTS FOR (n:OntologyProperty) ON (n.id)");
+                indexCommands.add("CREATE TEXT INDEX individual_id IF NOT EXISTS FOR (n:OntologyIndividual) ON (n.id)");
+                indexCommands.add("CREATE TEXT INDEX ontology_ont_id IF NOT EXISTS FOR (n:Ontology) ON (n.ontologyId)");
+                indexCommands.add("CREATE TEXT INDEX entity_ont_id IF NOT EXISTS FOR (n:OntologyEntity) ON (n.ontologyId)");
+                indexCommands.add("CREATE TEXT INDEX class_ont_id IF NOT EXISTS FOR (n:OntologyClass) ON (n.ontologyId)");
+                indexCommands.add("CREATE TEXT INDEX property_ont_id IF NOT EXISTS FOR (n:OntologyProperty) ON (n.ontologyId)");
+                indexCommands.add("CREATE TEXT INDEX individual_ont_id IF NOT EXISTS FOR (n:OntologyIndividual) ON (n.ontologyId)");
+                for (String command : indexCommands)
+                    try{
+                        session.run(command);
+                    } catch(Exception e){
+                        System.out.println("Could not create constraint: "+e.getMessage());
+                    }
+
                 System.out.println("Start Neo4J Modification...");
                 if(cmd.hasOption("i")){
                     File dir = new File(directory);
                     List<File> files = listFiles(dir.listFiles());
-                    if(cmd.hasOption("s"))
-                        generateCreationQueries(files,session,true);
-                    else
-                        generateCreationQueries(files,session,false);
+                    executeBatchedNodeQueries(files, session,batchSize);
+                    executeBatchedRelationshipQueries(files,session,batchSize);
                 } else
                     for(String ontology : ontologiesToBeRemoved.split(","))
                         try {
@@ -151,7 +161,6 @@ public class ImportCSV {
                         } catch (Exception e){
                             e.printStackTrace();
                         }
-
             }
         }
     }

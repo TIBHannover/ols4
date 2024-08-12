48c8faccf812de006446c3d4d0f30fdceec8eee5
Erhun Giray TUNCAY <48091473+giraygi@users.noreply.github.com>
2024-08-12 12:32:39 +0200

    implemented routine for array type query along with string type queries for #30#

--- dataload/csv2neo/src/main/java/uk/ac/ebi/spot/csv2neo/QueryGeneration.java
+++ dataload/csv2neo/src/main/java/uk/ac/ebi/spot/csv2neo/QueryGeneration.java
@@ -2,8 +2,6 @@ package uk.ac.ebi.spot.csv2neo;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * @author Erhun Giray TUNCAY
@@ -15,12 +13,10 @@ public class QueryGeneration {
     public static String generateBlankNodeCreationQuery(String[] titles, String[] values){
         StringBuilder sb = new StringBuilder();
         if (titles.length == values.length) {
-
             sb.append("CREATE (")
                     .append(":")
                     .append("`"+values[1].replace("|","`:`")+"`")
                     .append(" $props")
-
                     .append(")")
                     .append(" ");
         } else {
@@ -31,21 +27,23 @@ public class QueryGeneration {
     }
 
     public static Map<String,Object> generateProps(String[] titles, String[] values){
-
         Map<String,Object> props = new HashMap<>();
         props.put("id",values[0]);
+        props.put("_json",values[2]);
         if (titles.length == values.length) {
-            for (int i = 2; i < values.length; i++)
-                props.put(titles[i].split(":")[0].replaceAll("\"\"","\""),convertToJSONArray(values[i]));
-
+            for (int i = 3; i < values.length; i++){
+                String[] title = titles[i].split(":");
+                if (title.length > 1 && title[1].equals("string[]")) {
+                    props.put(title[0].replaceAll("\"\"","\""),values[i].split("\\|"));
+                } else
+                    props.put(title[0].replaceAll("\"\"","\""),values[i]);
+            }
         } else {
             System.out.println("titles and values are not equal");
             System.out.println("titles: "+titles.length + " - values: " +values.length);
         }
-
         Map<String,Object> params = new HashMap<>();
         params.put( "props", props );
-
         return params;
     }
 
@@ -56,7 +54,7 @@ public class QueryGeneration {
             sb.append("MATCH (n"+idToLabel(values[0])+" {id: "+"\'"+values[0]+"\'"+"}),")
                     .append("(m"+idToLabel(values[2])+" {id: "+"\'"+values[2]+"\'"+"}) ")
                     .append("WHERE n.id STARTS WITH '"+values[0].split("\\+")[0]+"' AND m.id STARTS WITH '"+values[2].split("\\+")[0]+"' ")
-                    .append("AND n.ontologyId = '"+values[0].split("\\+")[0]+"' AND m.ontologyId = '"+values[2].split("\\+")[0]+"' ")
+                    .append("AND '"+values[0].split("\\+")[0]+"' IN n.ontologyId AND '"+values[2].split("\\+")[0]+"' IN m.ontologyId ")
                     .append("CREATE (n)-[:")
                     .append("`"+values[1].replace("|","`:`")+"`")
                     .append("]->(m)");
@@ -68,54 +66,8 @@ public class QueryGeneration {
         return sb.toString();
     }
 
-    public static String generateRelationCreationQuery2(String[] titles, String[] values){
-        StringBuilder sb = new StringBuilder();
-        if (titles.length == values.length){
-            sb.append("MATCH (n {id: "+"\'"+values[0]+"\'"+"})-[:")
-                    .append("`"+values[1].replace("|","`:`")+"`")
-                    .append("]->(m {id: "+"\'"+values[2]+"\'"+"})");
-        }
-
-        return sb.toString();
-    }
-
     public static String generateOntologyDeleteQuery(String ontologyPrefix){
         return "MATCH (n) where n.id STARTS WITH '"+ontologyPrefix+"' DETACH DELETE n";
-    }
-
-    public static String convertToJSONArray(String input){
-        if (input.contains("|")){
-            input = input.substring(1,input.length()-1);
-            String[] sarray = input.split("\\|");
-            StringBuilder sb = new StringBuilder();
-            sb.append("[");
-            for (int i = 0;i<sarray.length;i++){
-                sb.append("\'").append(sarray[i]).append("\'");
-                if(i< sarray.length -1)
-                    sb.append(",");
-
-            }
-            sb.append("]");
-            input = sb.toString();
-        }
-
-        return input;
-    }
-
-    public static String decode(String input) {
-        Pattern pattern = Pattern.compile("\\\\u[0-9a-fA-F]{4}");
-        Matcher matcher = pattern.matcher(input);
-
-        StringBuilder decodedString = new StringBuilder();
-
-        while (matcher.find()) {
-            String unicodeSequence = matcher.group();
-            char unicodeChar = (char) Integer.parseInt(unicodeSequence.substring(2), 16);
-            matcher.appendReplacement(decodedString, Character.toString(unicodeChar));
-        }
-
-        matcher.appendTail(decodedString);
-        return decodedString.toString();
     }
 
     public static String idToLabel(String id){

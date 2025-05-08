28a465c8c0a0d85a5bcca3753f45095ace137f6c
deepananbalagan <deepanbtechceg@gmail.com>
2025-05-08 16:26:43 +0200

    added grouping to apis

--- backend/src/main/java/uk/ac/ebi/spot/ols/config/SwaggerConfig.java
+++ backend/src/main/java/uk/ac/ebi/spot/ols/config/SwaggerConfig.java
@@ -4,6 +4,8 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.servers.Server;
+
+import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
@@ -32,6 +34,26 @@ public class SwaggerConfig {
                         )
                 );
     }
+    
+    @Bean
+    public GroupedOpenApi v1Api() {
+        return GroupedOpenApi.builder()
+        		   .group("version 1")
+        		   .packagesToScan("uk.ac.ebi.spot.ols.controller.api.v1")
+        		   .build();
+    }
+    
+    @Bean
+    public GroupedOpenApi v2Api() {
+        return GroupedOpenApi.builder()
+        		   .group("version 2")
+        		   .packagesToScan(
+        				   "uk.ac.ebi.spot.ols.controller.api.v2",
+        				   "uk.ac.ebi.spot.ols.reststatistics.controller"
+        			)
+        		   .build();
+    }
+    
 
 }
 
